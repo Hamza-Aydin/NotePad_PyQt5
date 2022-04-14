dacd84c8ba905da6f49c72517fbdcf2e2b1aff2c
new.yazilimci <96042394+newyazilimci@users.noreply.github.com>
2022-04-14 22:40:59 +0300

    Update preje2_Notepad.py

--- preje2_Notepad.py
+++ preje2_Notepad.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from PyQt5.QtWidgets import QWidget,QApplication,QTextEdit,QLabel,QPushButton,QVBoxLayout,QFileDialog,QHBoxLayout
+from PyQt5.QtWidgets import QApplication,qApp,QMainWindow,QAction
 
 class Notepad(QWidget):
 
@@ -11,7 +12,7 @@ class Notepad(QWidget):
 
     def ana_fonksiyon(self):
 
-        self.yazı=QTextEdit()
+        self.yazi=QTextEdit()
         self.buton1=QPushButton("TEMİZLE")
         self.buton2=QPushButton("DOSYA AÇ")
         self.buton3=QPushButton("KAYDET")
@@ -25,7 +26,7 @@ class Notepad(QWidget):
 
         v_box=QVBoxLayout()
 
-        v_box.addWidget(self.yazı)
+        v_box.addWidget(self.yazi)
         v_box.addLayout(h_box)
 
         self.setLayout(v_box)
@@ -36,11 +37,9 @@ class Notepad(QWidget):
         self.buton2.clicked.connect(self.dosya_ac)
         self.buton3.clicked.connect(self.dosya_kaydet)
 
-        self.show()
-
     def yaziyi_sil(self):
 
-        self.yazı.clear()
+        self.yazi.clear()
 
     def dosya_ac(self):
 
@@ -49,7 +48,7 @@ class Notepad(QWidget):
         # ve bu metot bize seçtiğimiz dosyanın bulunduğu konumu demet olarak geri döndürür.
 
         with open(dosya_ismi[0],"r") as file:
-            self.yazı.setText(file.read())
+            self.yazi.setText(file.read())
 
     def dosya_kaydet(self):
 
@@ -57,10 +56,61 @@ class Notepad(QWidget):
         #Bu sefer kaydedilecek yerin konumu demet olarak döndürecek
 
         with open(dosya_ismi[0],"w") as file:
-            file.write(self.yazı.toPlainText())
+            file.write(self.yazi.toPlainText())
 
+class menu(QMainWindow):
+
+    def __init__(self):
+
+        super().__init__()
+
+        self.pencere=Notepad()
+
+        self.setCentralWidget(self.pencere)
+
+        self.menuleri_olustur()
+
+    def menuleri_olustur(self):
+
+        menubar=self.menuBar()
+
+        dosya=menubar.addMenu("dosya")
+
+        dosya_ac=QAction("dosya aç",self)
+        dosya_ac.setShortcut("CTRL+O")
+
+        dosya_kaydet = QAction("dosya kaydet", self)
+        dosya_kaydet.setShortcut("CTRL+S")
+
+        dosya_temizle= QAction("dosya temizle", self)
+        dosya_temizle.setShortcut("CTRL+D")
+
+        cıkıs = QAction("çıkış", self)
+        cıkıs.setShortcut("CTRL+Q")
+
+        dosya.addAction(dosya_ac)
+        dosya.addAction(dosya_kaydet)
+        dosya.addAction(dosya_temizle)
+        dosya.addAction(cıkıs)
+
+        self.setWindowTitle("Gelişmiş_NotePad")
+        self.setGeometry(750,250,500,500)
+        dosya.triggered.connect(self.metotlar)
+        self.show()
+
+    def metotlar(self,action):
+
+        if action.text()=="dosya aç":
+            self.pencere.dosya_ac()
+        if action.text()=="dosya kaydet":
+            self.pencere.dosya_kaydet()
+        if action.text()=="dosya temizle":
+            self.pencere.yaziyi_sil()
+        if action.text()=="çıkış":
+            qApp.quit()
 
 app=QApplication(sys.argv)
-a=Notepad()
+a=menu()
 sys.exit(app.exec_())
+
 
